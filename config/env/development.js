6e46541884ee484847805d3bbde5f5a646943b1c
Chris Troutner <chris.troutner@gmail.com>
2021-01-13 13:21:56 -0800

    Using mainnet WIF

--- config/env/development.js
+++ config/env/development.js
@@ -3,5 +3,5 @@ module.exports = {
   networkURL: process.env.RESTURL ? process.env.RESTURL : 'https://api.fullstack.cash/v4/',
   network: 'mainnet',
   tokenID: process.env.TOKENID ? process.env.TOKENID : '622e97e82a13ee5bc2ab186b08449bd4be1bffae3feda6d47dc6e612791eb47b',
-  WIF: process.env.WIF ? process.env.WIF : 'cQFUiXart4QQhon5HMErU6aMsXBEUHwTHAwJT5fh388Txa1pwvXM'
+  WIF: process.env.WIF ? process.env.WIF : 'L5EDpCV9UURj6FhMo7CZk4QsH95orcVsm2qjKirngaXgeA3A6ZhZ'
 }
