4b7ae9cac7753d1ef8d6f6224cdfc7158b5d0dff
Chris Troutner <chris.troutner@gmail.com>
2021-01-13 13:19:39 -0800

    Adding default WIF

--- config/env/development.js
+++ config/env/development.js
@@ -3,5 +3,5 @@ module.exports = {
   networkURL: process.env.RESTURL ? process.env.RESTURL : 'https://api.fullstack.cash/v4/',
   network: 'mainnet',
   tokenID: process.env.TOKENID ? process.env.TOKENID : '622e97e82a13ee5bc2ab186b08449bd4be1bffae3feda6d47dc6e612791eb47b',
-  WIF: process.env.WIF
+  WIF: process.env.WIF ? process.env.WIF : 'cQFUiXart4QQhon5HMErU6aMsXBEUHwTHAwJT5fh388Txa1pwvXM'
 }
