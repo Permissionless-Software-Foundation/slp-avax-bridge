c691eee5a1b9ecc6b5933ba9ad1a241674eb7b65
Gary Nadir <nadirgary@gmail.com>
2021-01-16 13:44:55 -0400

    Added method for burning slp tokens

--- test/integration/bch-integration.js
+++ test/integration/bch-integration.js
@@ -11,9 +11,24 @@ describe('#bch.js', () => {
   describe('#mintSlp', () => {
     it('should complete successfully and mint the tokens', async () => {
       try {
-        const num = 5
+        const num = 10
         const txid = await uut.mintSlp(num)
         // console.log(`txid: ${JSON.stringify(txid, null, 2)}`)
+
+        assert.typeOf(txid, 'string')
+      } catch (err) {
+        console.log(err)
+        assert.fail('unexpected result')
+      }
+    })
+  })
+
+  describe('#burnSlp', () => {
+    it('should burn a given number of tokens', async () => {
+      try {
+        const num = 121
+        const txid = await uut.burnSlp(num)
+        // console.log(`txid: ${JSON.stringify(txid, null, 2)}`)f
 
         assert.typeOf(txid, 'string')
       } catch (err) {
