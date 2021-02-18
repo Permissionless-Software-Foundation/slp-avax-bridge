0c31b292591d28badbac71470ca4bc96fd5ab3a7
Gary Nadir <nadirgary@gmail.com>
2021-02-18 16:14:17 -0400

    fix(config): Masked variables in config file

--- config/env/development.js
+++ config/env/development.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: 'dev',
   network: 'mainnet',
-  networkURL: process.env.RESTURL || 'https://api.fullstack.cash/v4/',
-  tokenID: process.env.TOKENID || '600ee24d0f208aebc2bdd2c4ee1b9acb6d57343561442e8676b5bbea311d5a0f',
-  WIF: process.env.WIF || 'L5EDpCV9UURj6FhMo7CZk4QsH95orcVsm2qjKirngaXgeA3A6ZhZ',
+  networkURL: process.env.RESTURL,
+  tokenID: process.env.TOKENID,
+  WIF: process.env.WIF,
 
-  AVAX_IP: process.env.AVAX_IP || '78.47.131.51',
-  AVAX_PORT: process.env.AVAX_PORT || '9650',
-  AVAX_PRIVATE_KEY: process.env.AVAX_PRIVATE_KEY || 'PrivateKey-xRXf1B6RotiCrHDn5QGhpwJs9wwPJVhn4JptnZLq27pqf92LW',
-  AVAX_TOKEN: process.env.AVAX_TOKEN || '1MWQgvmpeb6nsprdyaCRP3P5qnmbEDLRPzXYmKY5ySpuciGRU'
+  AVAX_IP: process.env.AVAX_IP,
+  AVAX_PORT: process.env.AVAX_PORT,
+  AVAX_PRIVATE_KEY: process.env.AVAX_PRIVATE_KEY,
+  AVAX_TOKEN: process.env.AVAX_TOKEN,
 }
