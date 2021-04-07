f8230df3096dcc223c351b27dea3983cbfe04718
Gary Nadir <nadirgary@gmail.com>
2021-04-07 00:56:36 -0400

    feat(mint): Added optional address parameter in minting methods

--- config/index.js
+++ config/index.js
@@ -4,9 +4,5 @@ const common = require('./env/common')
 const env = process.env.BCH_ENV || 'development'
 const route = path.join(__dirname, 'env', env.trim())
 const config = require(route)
-// trim the space at the end of variables in windows
-for (const key in config) {
-  config[key] = config[key] ? config[key].trim() : config[key]
-}
 
 module.exports = module.exports = Object.assign({}, common, config)
